9bf6c3f7afc1873064d1f9d57b876abaebbec00c
Jar <lumapasjarield@gmail.com>
2026-02-13 09:31:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ function showMessage() {
 function closeMessage() {
   document.getElementById("popup").style.display = "none";
 }
+const container = document.querySelector(".fireflies");
+
+const numberOfFireflies = 70; // Increase this if you want even more
+
+for (let i = 0; i < numberOfFireflies; i++) {
+  const firefly = document.createElement("div");
+  firefly.classList.add("firefly");
+
+  firefly.style.left = Math.random() * 100 + "vw";
+
+  const duration = 8 + Math.random() * 12;
+  firefly.style.animation = `
+    floatUp ${duration}s linear infinite,
+    flicker ${2 + Math.random() * 3}s ease-in-out infinite alternate
+  `;
+
+  firefly.style.animationDelay = Math.random() * 10 + "s";
+
+  const size = 3 + Math.random() * 4;
+  firefly.style.width = size + "px";
+  firefly.style.height = size + "px";
+
+  container.appendChild(firefly);
+}
